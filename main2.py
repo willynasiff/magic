84a3c1a4628dde107233e41c4e8deb42974736c5
willynasiff <willynasiff@gmail.com>
2017-04-11 14:56:20 -0300

    dos columnas en la tabla

--- main2.py
+++ main2.py
@@ -21,5 +21,5 @@ def b(number):
 
 
 if __name__=='__main__':
-  app.run()
+  app.run(host='0.0.0.0')
 
--- mostrar2.py
+++ mostrar2.py
@@ -8,30 +8,58 @@ def mostrarCartas(requestW):
   c = miDB.cursor()
 
   retorno = []
+  retorno2 = []
+  laLista = []
   mapa = []
-  
+  agregar2 = []
+  agregar = []
+
 
   query = 'select Carta.Nombre, Carta.ID, count(Carta.ID) from Carta, Carta_Mazo where Carta.ID = Carta_Mazo.ID_Carta and Carta_Mazo.ID_Mazo = {} group by Carta.ID order by count(Carta.ID);'.format(requestW)
 
   for row in c.execute(query):
-    nombre = row[0]
-    ID = row[1]
-    cantidad = row[2]
-
-    agregar2 = "/static/fotos/{}.png".format(ID)
-
-    agregar = "{1} {0} cantidad: {2}".format(nombre, ID, cantidad)
-    retorno.append(agregar)
-    retorno.append(agregar2)
-  
- 
+    laLista.append(row)
 
   miDB.commit()
   miDB.close()
+ 
+  if len(laLista)%2 == 1:
+    final = len(laLista) -1
+  else:
+    final = len(laLista)
 
+  for row in range(0, final, 2):
+    sacar = laLista[row]
+    sacar2 = laLista[row+1]   
+ 
+    nombre = sacar[0]
+    ID = sacar[1]
+    cantidad = sacar[2]
+
+    nombre2 = sacar2[0]
+    ID2 = sacar2[1]
+    cantidad2 = sacar2[2]
+
+    agregar = ["/static/fotos/{}.png".format(ID), "/static/fotos/{}.png".format(ID2)]
+    agregar2 = ["{1} {0} cantidad: {2}".format(nombre, ID, cantidad), "{1} {0} cantidad: {2}".format(nombre2, ID2, cantidad2)]
+
+    retorno.append(agregar)
+    retorno.append(agregar2)
     
+ 
+  if len(laLista)%2 == 1:
+    imparSacar = laLista[row+2]
 
+    nombreImpar = imparSacar[0]
+    IDimpar = imparSacar[1]
+    cantidadImpar = imparSacar[2]
 
+    agregar = ["/static/fotos/{}.png".format(IDimpar), "/static/fotos/0.png"]
+    agregar2 = ["{1} {0} cantidad: {2}".format(nombreImpar, IDimpar, cantidadImpar), ""] 
 
+    retorno.append(agregar)
+    retorno.append(agregar2)
+      
+ 
   return retorno
   
